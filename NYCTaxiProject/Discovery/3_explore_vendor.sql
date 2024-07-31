1c878905ff6d6bcaac420289f8fd1b0c77b86430
selkays <salehkays@gmail.com>
2024-07-31 13:57:01 -0400

    Add files via upload

---
+++ NYCTaxiProject/Discovery/3_explore_vendor.sql
@@ -0,0 +1,22 @@
+USE nyc_taxi_discovery
+
+SELECT *
+FROM
+    OPENROWSET(
+        BULK 'vendor_unquoted.csv',
+        DATA_SOURCE = 'nyc_taxi_data_raw',
+        FORMAT = 'CSV',
+        PARSER_VERSION = '2.0',
+        HEADER_ROW = TRUE
+    ) AS vendor;
+
+SELECT *
+FROM
+    OPENROWSET(
+        BULK 'vendor.csv',
+        DATA_SOURCE = 'nyc_taxi_data_raw',
+        FORMAT = 'CSV',
+        PARSER_VERSION = '2.0',
+        HEADER_ROW = TRUE
+    ) AS vendor;
+
